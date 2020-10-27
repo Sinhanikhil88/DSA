e0e320dc0f237dc1dd73c74bef286bccbd5f5eb1
Sinhanikhil88 <sinhaakshay77377@gmail.com>
2020-10-27 08:33:24 -0700

    Create CheckGreaterAlphabet.cpp

---
+++ Cpp/WarmUp/CheckGreaterAlphabet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i,j;
+    cout << "Enter two numbers that needs to be compare\n";
+    cin >> i >> j;
+    switch (i>j)
+    {
+    case 0:
+     cout << j;
+        break;
+    case 1:
+    cout << i;
+    break ;
+    
+    }
+}
