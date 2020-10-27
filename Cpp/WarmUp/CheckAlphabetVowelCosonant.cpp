be986ac51d5f12be2fcc7ea77d119ae116f61f19
Sinhanikhil88 <sinhaakshay77377@gmail.com>
2020-10-27 08:16:21 -0700

    Create CheckAlphabetVowelCosonant.cpp

---
+++ Cpp/WarmUp/CheckAlphabetVowelCosonant.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    char i;
+    cout << "Enter an alphabet\n";
+    cin >> i;
+    switch (i)
+    {
+    case 'a':
+    cout << "It is a vowel";
+        break;
+        case 'e':
+    cout << "It is a vowel";
+        break;
+     case 'i':
+    cout << "It is a vowel";
+        break;
+     case 'o':
+    cout << "It is a vowel";
+        break;
+     case 'u':
+    cout << "It is a vowel";
+        break;
+
+    
+    default:
+    cout << "It is a consonant";
+        break;
+    }
+}
