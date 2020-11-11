998b71162d5a31bd5fbfa9c8f6dccd734954af51
Sinhanikhil88 <sinhaakshay77377@gmail.com>
2020-11-12 03:17:26 +0530

    Create SumOfDigits.cpp

---
+++ Cpp/WarmUp/SumOfDigits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n ,l,sum =0;
+    cout << " Enter a Number\n";
+    cin >> n;
+    while (n/10!=0){
+        l = n%10; 
+        sum = sum + l;                                                  
+       n = n /10;                                                       
+    }
+    sum = sum +n;
+    cout << sum;
+}
