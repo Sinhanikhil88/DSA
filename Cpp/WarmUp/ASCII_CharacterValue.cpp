94c12dc5c65b9206118c1cdaae2f2a8645fe2df2
Sinhanikhil88 <sinhaakshay77377@gmail.com>
2020-12-11 00:54:29 +0530

    Create ASCII_CharacterValue.cpp

---
+++ Cpp/WarmUp/ASCII_CharacterValue.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a = 'a';
+    char ab = 'a';
+    for(int i=1;i<=26;i++){
+        cout << "the ASCII value of the character "<< ab << " =  ";
+        cout << a <<"\n";
+        a++;
+        ab++;
+    }
+}
